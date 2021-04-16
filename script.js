ea07ded3f0ca6d0f64e337108c9cc996001428f6
EK.BOSMAN <81225655+ekbosman@users.noreply.github.com>
2021-04-16 09:02:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,223 +1,99 @@
-// Quizsida //
+// Grids och items //
 
-// H1-interaktion till ett klickat svar. //
-
-var fact = document.getElementById("fact");
-var explaination = document.getElementById("explaination");
-var question = document.getElementById("question");
-var grid6 = document.getElementsByClassName("grid6");
-var grid5 = document.getElementsByClassName("grid5");
-var grid4 = document.getElementsByClassName("grid4");
-var grid3 = document.getElementsByClassName("grid3");
-var grid2 = document.getElementsByClassName("grid2");
 var grid1 = document.getElementsByClassName("grid");
-var svar1 = document.getElementsByClassName("g6");
-var svar2 = document.getElementsByClassName("g7");
-var svar3 = document.getElementsByClassName("g8");
-var svar4 = document.getElementsByClassName("g9");
-var svar5 = document.getElementsByClassName("g10");
-var g11 = document.getElementsByClassName("g11");
-var g11i1 = document.getElementsByClassName("g11i1");
-var g11i2 = document.getElementsByClassName("g11i2");
-var g12 = document.getElementsByClassName("g12");
-var g12i1 = document.getElementsByClassName("g12i1");
-var g12i2 = document.getElementsByClassName("g12i2");
-var g13 = document.getElementsByClassName("g13");
-var btnnext = document.getElementById("btn-next");
-var btnforward = document.getElementById("btn-forward");
+var grid2 = document.getElementsByClassName("grid2");
+var grid3 = document.getElementById("result");
 
-// H1-interaktion till ett klickat svar. //
+// Byter från startsida till quizsida //
 
-function klickad() {
-  explaination.innerHTML = "";
-  svar1[0].style.display = "none";
-  svar2[0].style.display = "none";
-  svar3[0].style.display = "none";
-  svar4[0].style.display = "none";
-  svar5[0].style.visibility = "visible";
+function start() {
+  grid1[0].style.display = "none";
+  grid1[0].style.WebkitAnimation = "fejdut 2s";
+  grid2[0].style.display = "grid";
+  grid2[0].style.animation = "fejdin 2s";
 }
 
-// H1-interaktion för hovering över svar. //
+// Mejlar Ivan ett förslag //
 
-function solitary() {
-    
-  explaination.innerHTML = "Alone time, for reflection and self care seems the most evident.";
-}
+// Här kan jag lägga till fler formulär //
+// Dessa hämtar var formulär baserat på deras id -- behöver döpas om.//
 
-function opposite() {
-  explaination.innerHTML = "Something requiring a mindset in complete opposite his, challenging him on a social level.";
-}
+const form1 = document.querySelector("#form1");
+const result = document.querySelector("#result");
 
-function brainy() {
-  explaination.innerHTML = "Logical and puzzley, something that helps him problem solving without attaching emotions to it.";
+// Här så göms och visas olika formulr vid submit: den sista ska ha count() på sig. //
 
-}
+form1.onsubmit = function(e) {
+  // E.prevent förhindrar att en kan skicka in formuläret innan allt är tickat. Du kan styla meddelandet. //
 
-function physical() {
+  e.preventDefault();
+  form1.classList.add("hidden");
+  result.classList.remove("hidden");
+  count();
+  result.innerHTML = scoreResult; scoreMessage;
+};
+
+// Här deklareras de olika kategorierna samt deras värden som är nollstälda och adderas sedan efter prestation//
+
+// Här deklareras mejlen och subject line (dessa är alltid densamma) för att fyllas in automatiskt i mejlutskicket//
+
+const email = "ivan.gedin@edu.beckmans.se";
+const subject = "Dearest Ivan, I think you need a hobby…";
+let scoreMessage = "";
+let scoreResult ="";
+
+// Funktionen som kollar att allt är 'checked', sedan switchar mellan de olika kategorierna för att mäta deras antal//
+
+function count() {
+  let answers = document.querySelectorAll("input:checked");
+
+  let scoreA = 0;
+  let scoreB = 0;
+  let scoreC = 0;
+
+  for (let answer of answers) {
+    switch (answer.value) {
+      case "a":
+        scoreA++;
+        break;
+      case "b":
+        scoreB++;
+        break;
+      case "c":
+        scoreC++;
+        break;
+    }
+  }
   
-  explaination.innerHTML = "Something to get him pumped up and ripped like Michael Phelps.";
- 
-}
+  // Funktionen som kollar att allt är 'checked', sedan switchar mellan de olika kategorierna för att mäta deras antal//
 
-// Gömmer första svarselemenet. //
-
-function göms1() {
-  svar2[0].style.visibility = "hidden";
-  svar3[0].style.visibility = "hidden";
-  svar4[0].style.visibility = "hidden";
-  svar1[0].style.gridColumnStart = "1";
-  svar1[0].style.gridColumnEnd = "4";
-  svar1[0].style.gridRowStart = "2";
-  svar1[0].style.gridRowEnd = "4";
-  svar1[0].style.background = "#61ef64";
-  svar1[0].style.color = "white";
-  btnforward.style.WebkitAnimation = "blinka 2s infinite";
-  question.innerHTML = "Solitude might be the way to biding time better.";
-  fact.innerHTML = "Alone time, groovy.";
-  window.scrollTo({
-  top: 0,
-  left: 0,
-  behavior: 'smooth'
-});
-}
-
-// Gömmer andra svarselemenet //
-
-function göms2() {
-  svar1[0].style.visibility = "hidden";
-  svar3[0].style.visibility = "hidden";
-  svar4[0].style.visibility = "hidden"; 
-  svar2[0].style.gridColumnStart = "1";
-  svar2[0].style.gridColumnEnd = "4";
-  svar2[0].style.gridRowStart = "2";
-  svar2[0].style.gridRowEnd = "4";
-  svar2[0].style.background = "#61ef64";
-  svar2[0].style.color = "white";
-  btnforward.style.WebkitAnimation = "blinka 2s infinite";
-  question.innerHTML = "<i>A new me<i> - perhaps by facing inconsequantial fears, he may grow to face real ones easily!";
-  fact.innerHTML = "Opposite schmopposite.";
-  window.scrollTo({
-  top: 0,
-  left: 0,
-  behavior: 'smooth'
-});
-}
-
-// Gömmer tredje svarselemenet. //
-
-
-function göms3() {
-  svar1[0].style.visibility = "hidden";
-  svar2[0].style.visibility = "hidden";
-  svar4[0].style.visibility = "hidden"; 
-  svar1[0].style.gridColumnStart = "1";
-  svar3[0].style.gridColumnEnd = "4";
-  svar3[0].style.gridRowStart = "2";
-  svar3[0].style.gridRowEnd = "4";
-  svar3[0].style.background = "#61ef64";
-  svar3[0].style.color = "white";
-  btnforward.style.WebkitAnimation = "blinka 2s infinite";
-  question.innerHTML = "The mind-body connection is inevitable. Imagine investing in one's mental abilities, and being in full control.";
-  fact.innerHTML = "Something mindboggeling, for sure.";
-  window.scrollTo({
-  top: 0,
-  left: 0,
-  behavior: 'smooth'
-});
-}
-
-// Gömmer fjärde svarselemenet. //
-
-function göms4() {
-  svar1[0].style.visibility = "hidden";
-  svar2[0].style.visibility = "hidden";
-  svar3[0].style.visibility = "hidden"; 
-  svar4[0].style.gridColumnStart = "1";
-  svar4[0].style.gridColumnEnd = "4";
-  svar4[0].style.gridRowStart = "2";
-  svar4[0].style.gridRowEnd = "4";
-  svar4[0].style.background = "#61ef64";
-  svar4[0].style.color = "white";
-  btnforward.style.WebkitAnimation = "blinka 2s infinite";
-  btnforward.style.boxShadow = "inset 0.5vw 0.5vh 1vw 1vh #61ef64";
-  question.innerHTML = "We are after all animals. Why not engage with our neglected physical instincts, and reap the evolutionary rewards!";
-  fact.innerHTML = "Let's make this guy Sporty Spice.";
-  window.scrollTo({
-  top: 0,
-  left: 0,
-  behavior: 'smooth'
-});
+  if (scoreA >= scoreB && scoreA >= scoreC) {
+    scoreResult = "Sprituality";
+    scoreMessage =
+      "Dear Ivan: Bless You, Ivan. You should stop looking for a hobby and start searching inwards for it: you are meant to be a leader with followers, someone to tell us when to meditate and what juices to drink. From what I've learned from basic facts about you, you the inner peace and empathy that it takes to commit to spirituality. What we all share is problem that is sympthomatic for the millennial and zoomer generation, where we to a greater extent invest meaning and identity in work. Don't be mistaken: finding fulfillment and self-realization in your work life is fantastic! However when we also stock our esteem and confidence in the outcome of what we produce, we will ultimately burn out having no substitute activity.  That's where hobbies come in. The healthy hobby is a distraction from what is monetary and dutiful. It involves active rest, in opposite to passive rest. When we find a hobby that suits us, we are able to blissfully let go off the anxieties produced by a late bill, a deteriorating relationship or an awkward run-in with an aquiantance. Because it is easier to give advice in third person, Ivan might even represent your own need for a hobby. He is just a projection map, really.";
   }
 
-// Byter från ett grid med innehåll till ett annat. //
+  if (scoreB >= scoreA && scoreB >= scoreC) {
+    scoreResult = "Cooking";
+    scoreMessage =
+      "Herr Ober Ivan! Yes chef! You should really dig out your pans and make the culinary your second love. From what I've learned from basic facts about you, you have what it takes to become a primo cook. What we all share is problem that is sympthomatic for the millennial and zoomer generation, where we to a greater extent invest meaning and identity in work. Don't be mistaken: finding fulfillment and self-realization in your work life is fantastic! However when we also stock our esteem and confidence in the outcome of what we produce, we will ultimately burn out having no substitute activity.  That's where hobbies come in. The healthy hobby is a distraction from what is monetary and dutiful. It involves active rest, in opposite to passive rest. When we find a hobby that suits us, we are able to blissfully let go off the anxieties produced by a late bill, a deteriorating relationship or an awkward run-in with an aquiantance. Because it is easier to give advice in third person, Ivan might even represent your own need for a hobby. He is just a projection map, really.";
+  }
 
+  if (scoreC >= scoreB && scoreC >= scoreA) {
+    scoreResult = "Triathlon";
+    scoreMessage =
+      "What's good Ivan! See you at 5 a.m., we're hitting the gym to pump some serious iron. Now don't be scared: from what I've learned from basic facts about you, you have what it takes to become a 100% meat-packing Michael Phelps kind of guy, with a mental health that is bullet proof. What we all share is problem that is sympthomatic for the millennial and zoomer generation, where we to a greater extent invest meaning and identity in work. Don't be mistaken: finding fulfillment and self-realization in your work life is fantastic! However when we also stock our esteem and confidence in the outcome of what we produce, we will ultimately burn out having no substitute activity.  That's where hobbies come in. The healthy hobby is a distraction from what is monetary and dutiful. It involves active rest, in opposite to passive rest. When we find a hobby that suits us, we are able to blissfully let go off the anxieties produced by a late bill, a deteriorating relationship or an awkward run-in with an aquiantance. Because it is easier to give advice in third person, Ivan might even represent your own need for a hobby. He is just a projection map, really.";
+  }
 
-function sidbyte1() {
-  grid1[0].style.visibility = "hidden";
-  grid1[0].style.display = "none"
-  grid1[0].style.WebkitAnimation = "fejdut 2s";
-  grid4[0].style.display= "grid";
-  grid4[0].style.animation = "fejdin 2s";
+  console.log(`A: ${scoreA}, B: ${scoreB} C: ${scoreC}`, scoreMessage);
+  console.log(`A: ${scoreA}, B: ${scoreB} C: ${scoreC}`, scoreResult);
+  document.querySelector("#btnSendEmail").disabled = false;
+  document.querySelector("#btnSendEmail").style.visibility = "visible";
 }
 
-function sidbyte2() {
-  grid4[0].style.visibility = "hidden";
-  grid4[0].style.display = "none";
-  grid4[0].style.animation = "fejdut 2s";
-  grid5[0].style.animation = "fejdin 2s";
-  grid5[0].style.visibility = "visible";
-  grid5[0].style.display= "grid";
+// Funktionen som skickar mejl och fyller i body utifrån olika resultat (går igenom storleksordningen på de olika kategorierna).//
+
+function sendEmail() {
+  let msg = `mailto:${email}?subject=${subject}&body=${scoreResult, scoreMessage}`;
+  window.open(msg);
 }
-
-function sida1() {
-  grid1[0].style.visibility = "hidden";
-  grid1[0].style.display = "none"
-  grid1[0].style.WebkitAnimation = "fejdut 2s";
-  grid1[0].style.animation = "fejdut 2s";
-  grid2[0].style.animationDelay = "5s";
-  grid2[0].style.WebkitAnimation = "fejdin 2s";
-  grid2[0].style.animation = "fejdin 2s";
-  grid2[0].style.visibility = "visible";
-  grid2[0].style.display = "grid";
-}
-
-// Byter från ett grid med innehåll till ett annat. //
-
-function sida2() {
-  grid2[0].style.visibility = "hidden";
-  grid2[0].style.display = "none";
-  grid3[0].style.display= "grid";
-  grid3[0].style.animation = "fejdin 2s";
-  g11[0].style.display = "grid";
-  g12[0].style.display = "grid";
-  g13[0].style.visiblity = "visible";
-}
-
-// Byter från ett grid med innehåll till ett annat. //
-
-function sida3() {
-  grid3[0].style.visibility = "hidden";
-  grid3[0].style.display = "none";
-  grid3[0].style.animation = "fejdin 2s";
-  grid4[0].style.visibility = "visible";
-  grid4[0].style.display= "grid";
-}
-
-function sida4() {
-  grid4[0].style.visibility = "hidden";
-  grid4[0].style.display = "none";
-  grid5[0].style.animation = "fejdin 2s";
-  grid5[0].style.visibility = "visible";
-  grid5[0].style.display= "grid";
-}
-
-
-function sida5() {
-  grid5[0].style.visibility = "hidden";
-  grid5[0].style.display = "none";
-  grid6[0].style.animation = "fejdin 2s";
-  grid6[0].style.visibility = "visible";
-  grid6[0].style.display= "grid";
-}
-
-
